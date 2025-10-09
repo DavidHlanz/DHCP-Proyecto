497bd25656978b5754a6c7c47b4ec6ee70db1474
David Ortiz <davidhlanz@gmail.com>
2025-10-09 11:09:11 +0200

    Final proyect

--- provision_servidor.sh
+++ provision_servidor.sh
@@ -2,3 +2,8 @@
 set -xeu
 apt-get update
 apt-get install -y isc-dhcp-server
+
+systemctl restart isc-dhcp-server
+
+cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
+mv /vagrant/vendor/dhcpd.conf /etc/dhcp/dhcpd.conf
