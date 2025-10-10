285e6d7a0583986cfb327ad54b0cc92ca42f6a87
David Ortiz <davidhlanz@gmail.com>
2025-10-10 09:20:45 +0200

    New Version Final

--- provision_servidor.sh
+++ provision_servidor.sh
@@ -6,4 +6,4 @@ apt-get install -y isc-dhcp-server
 systemctl restart isc-dhcp-server
 
 cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
-mv /vagrant/vendor/dhcpd.conf /etc/dhcp/dhcpd.conf
+cp -f /vagrant/vendor/dhcpd.conf /etc/dhcp/dhcpd.conf
